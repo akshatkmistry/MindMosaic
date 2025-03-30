7500674acd9c180448eec18ab0fac35083f10481
the._.akx <akshatkmistry8@gmail.com>
2025-03-30 23:23:54 +0530

    Modify Javascript code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-// Create and style the button
+
 let topButton = document.createElement('button');
 topButton.className = "scroll__button";
 topButton.innerText = "↑";
 
 document.body.appendChild(topButton);
 
-// Show the button when scrolling down
 window.onscroll = function() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         topButton.style.display = "block";
@@ -14,7 +13,6 @@ window.onscroll = function() {
     }
 };
 
-// Scroll to top function
 topButton.onclick = function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 };
